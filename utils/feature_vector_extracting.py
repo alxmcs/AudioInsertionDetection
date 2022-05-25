33f6034475beea08e1982386d0df3ac61a23e3c1
DmitriyTimoshkin <63157430+DmitriyTimoshkin@users.noreply.github.com>
2022-05-26 00:10:16 +0400

    CycleFileLoad
    Загрузка всех аудиофайлов из определенной директории

--- utils/feature_vector_extracting.py
+++ utils/feature_vector_extracting.py
@@ -4,6 +4,7 @@ import numpy as np
 import openpyxl
 import tensorflow as tf
 import time
+from os import listdir
 from pathlib import Path
 from datetime import datetime
 from tensorflow.keras import models
@@ -69,7 +70,7 @@ def process_audio(audio, rate, preproc, model=MODELS[0], weights='imagenet'):
     print(f'{datetime.now()}: got {result.shape[0]} {result[0].shape[0]}-dimensional vectors from {model}')
     return result
 
-def norm_calc(argument, net_mod, a1, s1, a2, s2):
+def norm_calc(net_mod, a1, s1, a2, s2):
     arr_1st_method_1 = process_audio(a1, s1, 'guzhov', net_mod)
     arr_2nd_method_1 = process_audio(a1, s1, 'palanisamy', net_mod)
     arr_1st_method_2 = process_audio(a2, s2, 'guzhov', net_model)
@@ -96,35 +97,59 @@ def norm_calc(argument, net_mod, a1, s1, a2, s2):
     sheet_1.append([np.mean(met2_1.tolist())])
     sheet_1.append(['palanisamy, different'])
     sheet_1.append([np.mean(met2_2.tolist())])
-    if (net_mod == 'ResNet'):
-        book.save(Path(Path.cwd(), 'docs', 'misc', 'ResNet_results.xlsx'))
-    if (net_mod == 'DenseNet'):
-        book.save(Path(Path.cwd(), 'docs', 'misc', 'DenseNet_results.xlsx'))
-    if (net_mod == 'Inception'):
-        book.save(Path(Path.cwd(), 'docs', 'misc', 'Inception_results.xlsx'))
+    book.save(Path(Path.cwd(), 'docs', 'misc', f'{net_mod}_results.xlsx'))
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="feature vector extracting script")
-    parser.add_argument("-r", dest="research", action='store_true', help="runs preset research if provided")
-    parser.add_argument("-m", dest="model", type=str, choices=MODELS, help="name of the model to use")
-    parser.add_argument("-w", dest="weights", type=str, default='imagenet', help="path to the model weights")
-    parser.add_argument("-a", dest="audio", type=str, help="path to an audio file")
-    parser.add_argument("-p", dest="preproc", type=str, choices=METHODS, default='palanisamy', help="name of the "
-                                                                                                    "preprocessing "
-                                                                                                    "method")
-    args = parser.parse_args()
-
-    #these two lines are needed to run inference on my dated gtx 1660 ti
-    #physical_devices = tf.config.experimental.list_physical_devices('GPU')
-    #tf.config.experimental.set_memory_growth(physical_devices[0], True)
+def time_for_diff_models():
     audio1, sr1 = load_audio(Path(Path.cwd(), 'docs', 'musicDS', '170439_argande102_wind-on-microphone.ogg'))
     audio2, sr2 = load_audio(Path(Path.cwd(), 'docs', 'musicDS', '401275_inspectorj_rain-moderate-c.ogg'))
     book = openpyxl.Workbook()
     sheet_1 = book.create_sheet("Results_of_testing_different_models", 0)
     sheet_1.append(['Метод предобработки', 'Модель нейросети', 'Среднее время обработки одного фрагмента фонограммы'])
     for net_model in MODELS:
-        norm_calc(args, net_model, audio1, sr1, audio2, sr2)
+        norm_calc(net_model, audio1, sr1, audio2, sr2)
+        startTime = time.time()
+        process_audio(audio1, sr1, 'guzhov', net_model)
+        endTime = time.time()
+        totalTime = endTime - startTime
+        print('Время выполнения для модели ' + net_model + ' методом "Guzhov et al." равно: ' + str(totalTime))
+        sheet_1.append(['Guzhov et al.', net_model, totalTime])
+        startTime = time.time()
+        process_audio(audio1, sr1, 'palanisamy', net_model)
+        endTime = time.time()
+        totalTime = endTime - startTime
+        print('Время выполнения для модели ' + net_model + ' методом "Palanisamy et al." равно: ' + str(totalTime))
+        sheet_1.append(['Palanisamy et al.', net_model, totalTime])
+    book.save(Path(Path.cwd(), 'docs', 'misc', 'Results_of_testing_different_models.xlsx'))
+
+
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser(description="feature vector extracting script")
+    # parser.add_argument("-r", dest="research", action='store_true', help="runs preset research if provided")
+    # parser.add_argument("-m", dest="model", type=str, choices=MODELS, help="name of the model to use")
+    # parser.add_argument("-w", dest="weights", type=str, default='imagenet', help="path to the model weights")
+    # parser.add_argument("-a", dest="audio", type=str, help="path to an audio file")
+    # parser.add_argument("-p", dest="preproc", type=str, choices=METHODS, default='palanisamy', help="name of the "
+    #                                                                                                 "preprocessing "
+    #                                                                                                 "method")
+    # args = parser.parse_args()
+
+    #these two lines are needed to run inference on my dated gtx 1660 ti
+    #physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    #tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    audio, sr = []
+    dir = Path(Path.cwd(), 'docs', 'musicDS')
+    files = listdir(dir)
+    book = openpyxl.Workbook()
+    sheet_1 = book.create_sheet("Results_of_testing_different_models", 0)
+    sheet_1.append(['Метод предобработки', 'Пара аудиозаписей', 'Модель нейросети', 'Среднее время обработки одного фрагмента фонограммы'])
+    for ind in len(files):
+        audio[ind], sr[ind] = load_audio(Path(Path.cwd(), 'docs', 'musicDS', f'{files[ind]}'))
+    
+    audio1, sr1 = load_audio(Path(Path.cwd(), 'docs', 'musicDS', '170439_argande102_wind-on-microphone.ogg'))
+    audio2, sr2 = load_audio(Path(Path.cwd(), 'docs', 'musicDS', '401275_inspectorj_rain-moderate-c.ogg'))
+    for net_model in MODELS:
+        norm_calc(net_model, audio1, sr1, audio2, sr2)
         startTime = time.time()
         process_audio(audio1, sr1, 'guzhov', net_model)
         endTime = time.time()
