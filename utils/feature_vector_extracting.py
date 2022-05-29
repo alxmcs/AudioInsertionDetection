7f629837873c3e77e9c21aa82ca2475758e40c6c
DmitriyTimoshkin <63157430+DmitriyTimoshkin@users.noreply.github.com>
2022-05-30 02:18:00 +0400

    Intermediate code
    Написал все до конца, нужно поправить ошибки и добавить проверку на индексы(сделаю 30.05.2022 утром)

--- utils/feature_vector_extracting.py
+++ utils/feature_vector_extracting.py
@@ -1,9 +1,12 @@
+from copy import copy
 import librosa
 import argparse
 import numpy as np
 import openpyxl
+from openpyxl import Workbook
 import tensorflow as tf
 import time
+import json
 from os import listdir
 from pathlib import Path
 from datetime import datetime
@@ -70,60 +73,78 @@ def process_audio(audio, rate, preproc, model=MODELS[0], weights='imagenet'):
     print(f'{datetime.now()}: got {result.shape[0]} {result[0].shape[0]}-dimensional vectors from {model}')
     return result
 
-def norm_calc(net_mod, a1, s1, a2, s2):
-    arr_1st_method_1 = process_audio(a1, s1, 'guzhov', net_mod)
-    arr_2nd_method_1 = process_audio(a1, s1, 'palanisamy', net_mod)
-    arr_1st_method_2 = process_audio(a2, s2, 'guzhov', net_model)
-    arr_2nd_method_2 = process_audio(a2, s2, 'palanisamy', net_model)
-    met1_1 = np.array([])
-    met1_2 = np.array([])
-    met2_1 = np.array([])
-    met2_2 = np.array([])
-    for n in range(11):
-        met1_1 = np.append(met1_1, np.linalg.norm(arr_1st_method_1[n] - arr_1st_method_1[n + 1]))
-        met1_1 = np.append(met1_1, np.linalg.norm(arr_1st_method_2[n] - arr_1st_method_2[n + 1]))
-        met1_2 = np.append(met1_2, np.linalg.norm(arr_1st_method_1[n] - arr_1st_method_2[n]))
-        met2_2 = np.append(met2_2, np.linalg.norm(arr_2nd_method_1[n] - arr_2nd_method_2[n]))
-        met2_1 = np.append(met2_1, np.linalg.norm(arr_2nd_method_1[n] - arr_2nd_method_1[n + 1]))
-        met2_1 = np.append(met2_1, np.linalg.norm(arr_2nd_method_2[n] - arr_2nd_method_2[n + 1]))
+def norm_calc(audios, trek):
+    dt = []
+    for fl in audios:
+        dt.append([load_audio(fl)]) 
+    for net_mod in MODELS:
+        book_tmp = openpyxl.Workbook()
+        mean_met1_1 = []
+        mean_met1_2 = []
+        mean_met2_1 = []
+        mean_met2_2 = [] 
+        for index_of_audios in range(len(dt) - 1):
+            sheet_1 = book.create_sheet(f"{net_mod}_results", 0)
+            arr_1st_method_1 = process_audio(dt[index_of_audios], dt[index_of_audios+1], 'guzhov', net_model)
+            arr_2nd_method_1 = process_audio(dt[index_of_audios], dt[index_of_audios+1], 'palanisamy', net_model)
+            arr_1st_method_2 = process_audio(dt[index_of_audios], dt[index_of_audios+1], 'guzhov', net_model)
+            arr_2nd_method_2 = process_audio(dt[index_of_audios], dt[index_of_audios+1], 'palanisamy', net_model)
+            met1_1 = np.array([])
+            met1_2 = np.array([])
+            met2_1 = np.array([])
+            met2_2 = np.array([])
+            for n in range(11):
+                met1_1 = np.append(met1_1, np.linalg.norm(arr_1st_method_1[n] - arr_1st_method_1[n + 1]))
+                met1_1 = np.append(met1_1, np.linalg.norm(arr_1st_method_2[n] - arr_1st_method_2[n + 1]))
+                met1_2 = np.append(met1_2, np.linalg.norm(arr_1st_method_1[n] - arr_1st_method_2[n]))
+                met2_2 = np.append(met2_2, np.linalg.norm(arr_2nd_method_1[n] - arr_2nd_method_2[n]))
+                met2_1 = np.append(met2_1, np.linalg.norm(arr_2nd_method_1[n] - arr_2nd_method_1[n + 1]))
+                met2_1 = np.append(met2_1, np.linalg.norm(arr_2nd_method_2[n] - arr_2nd_method_2[n + 1]))
+            mean_met1_1.append([np.mean(met1_1.tolist())])
+            mean_met1_2.append([np.mean(met1_2.tolist())])
+            mean_met2_1.append([np.mean(met2_1.tolist())])
+            mean_met2_2.append([np.mean(met2_2.tolist())])
+        sheet_1.append(['guzhov, same'])
+        sheet_1.append([np.mean(mean_met1_1.tolist())])
+        sheet_1.append(['guzhov, different'])
+        sheet_1.append([np.mean(mean_met1_2.tolist())])
+        sheet_1.append(['palanisamy, same'])
+        sheet_1.append([np.mean(mean_met2_1.tolist())])
+        sheet_1.append(['palanisamy, different'])
+        sheet_1.append([np.mean(mean_met2_2.tolist())])
+        book_tmp.save(f'{trek}'+f'{net_mod}_results.xlsx')
 
-    book = openpyxl.Workbook()
-    sheet_1 = book.create_sheet(f"{net_mod}_results", 0)
-    sheet_1.append(['guzhov, same'])
-    sheet_1.append([np.mean(met1_1.tolist())])
-    sheet_1.append(['guzhov, different'])
-    sheet_1.append([np.mean(met1_2.tolist())])
-    sheet_1.append(['palanisamy, same'])
-    sheet_1.append([np.mean(met2_1.tolist())])
-    sheet_1.append(['palanisamy, different'])
-    sheet_1.append([np.mean(met2_2.tolist())])
-    book.save(Path(Path.cwd(), 'docs', 'misc', f'{net_mod}_results.xlsx'))
-
-def time_for_diff_models():
-    audio1, sr1 = load_audio(Path(Path.cwd(), 'docs', 'musicDS', '170439_argande102_wind-on-microphone.ogg'))
-    audio2, sr2 = load_audio(Path(Path.cwd(), 'docs', 'musicDS', '401275_inspectorj_rain-moderate-c.ogg'))
+def time_for_diff_models(audios, trek):
+    dt = []
+    t1 = []
+    t2 = []
+    for fl in audios:
+        dt.append([load_audio(fl)])
     book = openpyxl.Workbook()
     sheet_1 = book.create_sheet("Results_of_testing_different_models", 0)
     sheet_1.append(['Метод предобработки', 'Модель нейросети', 'Среднее время обработки одного фрагмента фонограммы'])
-    for net_model in MODELS:
-        norm_calc(net_model, audio1, sr1, audio2, sr2)
-        startTime = time.time()
-        process_audio(audio1, sr1, 'guzhov', net_model)
-        endTime = time.time()
-        totalTime = endTime - startTime
-        print('Время выполнения для модели ' + net_model + ' методом "Guzhov et al." равно: ' + str(totalTime))
-        sheet_1.append(['Guzhov et al.', net_model, totalTime])
-        startTime = time.time()
-        process_audio(audio1, sr1, 'palanisamy', net_model)
-        endTime = time.time()
-        totalTime = endTime - startTime
-        print('Время выполнения для модели ' + net_model + ' методом "Palanisamy et al." равно: ' + str(totalTime))
-        sheet_1.append(['Palanisamy et al.', net_model, totalTime])
-    book.save(Path(Path.cwd(), 'docs', 'misc', 'Results_of_testing_different_models.xlsx'))
+    for index_of_audios in range(len(dt) - 1):
+        for net_model in MODELS:
+            startTime = time.time()
+            process_audio(dt[index_of_audios], dt[index_of_audios+1], 'guzhov', net_model)
+            endTime = time.time()
+            totalTime = endTime - startTime
+            print('Время выполнения для модели ' + net_model + ' методом "Guzhov et al." равно: ' + str(totalTime))
+            t1.append(totalTime)
+            startTime = time.time()
+            process_audio(dt[index_of_audios], dt[index_of_audios+1], 'palanisamy', net_model)
+            endTime = time.time()
+            totalTime = endTime - startTime
+            print('Время выполнения для модели ' + net_model + ' методом "Palanisamy et al." равно: ' + str(totalTime))
+            t2.append(totalTime)
+    t1.append(['Guzhov et al.', net_model, totalTime])
+    t2.append(['Palanisamy et al.', net_model, totalTime])
+    book.save(f'{trek}'+f'\Results_of_testing_different_models.xlsx')
 
 
 
 if __name__ == "__main__":
+    SETTINGS = 'C:\\Users\\stron\\Documents\\GitHub\\docs\\params\\main_args.json'
     # parser = argparse.ArgumentParser(description="feature vector extracting script")
     # parser.add_argument("-r", dest="research", action='store_true', help="runs preset research if provided")
     # parser.add_argument("-m", dest="model", type=str, choices=MODELS, help="name of the model to use")
@@ -137,29 +158,7 @@ if __name__ == "__main__":
     #these two lines are needed to run inference on my dated gtx 1660 ti
     #physical_devices = tf.config.experimental.list_physical_devices('GPU')
     #tf.config.experimental.set_memory_growth(physical_devices[0], True)
-    audio, sr = []
-    dir = Path(Path.cwd(), 'docs', 'musicDS')
-    files = listdir(dir)
-    book = openpyxl.Workbook()
-    sheet_1 = book.create_sheet("Results_of_testing_different_models", 0)
-    sheet_1.append(['Метод предобработки', 'Пара аудиозаписей', 'Модель нейросети', 'Среднее время обработки одного фрагмента фонограммы'])
-    for ind in len(files):
-        audio[ind], sr[ind] = load_audio(Path(Path.cwd(), 'docs', 'musicDS', f'{files[ind]}'))
-    
-    audio1, sr1 = load_audio(Path(Path.cwd(), 'docs', 'musicDS', '170439_argande102_wind-on-microphone.ogg'))
-    audio2, sr2 = load_audio(Path(Path.cwd(), 'docs', 'musicDS', '401275_inspectorj_rain-moderate-c.ogg'))
-    for net_model in MODELS:
-        norm_calc(net_model, audio1, sr1, audio2, sr2)
-        startTime = time.time()
-        process_audio(audio1, sr1, 'guzhov', net_model)
-        endTime = time.time()
-        totalTime = endTime - startTime
-        print('Время выполнения для модели ' + net_model + ' методом "Guzhov et al." равно: ' + str(totalTime))
-        sheet_1.append(['Guzhov et al.', net_model, totalTime])
-        startTime = time.time()
-        process_audio(audio1, sr1, 'palanisamy', net_model)
-        endTime = time.time()
-        totalTime = endTime - startTime
-        print('Время выполнения для модели ' + net_model + ' методом "Palanisamy et al." равно: ' + str(totalTime))
-        sheet_1.append(['Palanisamy et al.', net_model, totalTime])
-    book.save(Path(Path.cwd(), 'docs', 'misc', 'Results_of_testing_different_models.xlsx'))
+    with open(SETTINGS) as json_file:
+        settings = json.load(json_file)
+    time_for_diff_models(settings["audios"], settings["trek"])
+    norm_calc(settings["audios"], settings["trek"])
